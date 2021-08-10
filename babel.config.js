ae38f1ae9cd3afe0674394dff2bb2205ec9072a0
alveloper <alveloper@gmail.com>
2021-08-10 17:34:47 +0900

    choer: Install babel plugin module resolver

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,20 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@screens": "./src/screens",
+            "@Components": "./src/components",
+            "@utils": "./src/utils",
+            "@contexts": "./src/contexts",
+            "@config": "./src/config",
+            "@assets": "./assets"
+          }
+        }
+      ]
+    ]
   };
 };
---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { Game, Home } from "@screens";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image source={require("../assets/icon.png")} />
+      <Home />
+      <Game />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
