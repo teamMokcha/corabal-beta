c041dd60275b66904c3e15b6f75bb15033e2839b
hyejin yang <myosotis-0@naver.com>
2021-08-11 16:14:43 +0900

    refactor : Bootstrap the index.tsx

--- src/screens/intro/intro.tsx
+++ src/screens/intro/intro.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import styles from "./intro.styles";
+import { Text } from "@Components";
+import { ReactElement } from "react";
 
-export default function Intro() {
+export default function Intro(): ReactElement {
   return (
-    <View>
-      <Text>Hey this is Intro page</Text>
+    <View style={styles.container}>
+      <Text style={{ fontSize: 25 }} weight="700">
+        Hey this is Intro page
+      </Text>
     </View>
   );
 }
