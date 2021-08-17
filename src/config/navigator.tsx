21f95159a66d704fb8e475a3c1673a3244ac063c
hyejin yang <myosotis-0@naver.com>
2021-08-17 13:16:31 +0900

    feat : Implement navigation logic on intro page

--- src/config/navigator.tsx
+++ src/config/navigator.tsx
@@ -1,7 +1,15 @@
 import React, { ReactElement } from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Intro, Login, SignIn } from "@screens";
+
+const initialTheme = {
+  dark: false,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "white"
+  }
+};
 
 export type StackNavigatorParams = {
   Intro: undefined;
@@ -13,8 +21,11 @@ const Stack = createNativeStackNavigator<StackNavigatorParams>();
 
 export default function Navigator(): ReactElement {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
+    // 네비게이션을 2개의 컨테이너 or 스택으로 나눠야 할 수도 있음
+    // 1) 헤더가 보이지 않는 영역 - 인트로, 회원가입, 로그인
+    // 2 헤더가 보이는 영역 - 인덱스 등
+    <NavigationContainer theme={initialTheme}>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Intro" component={Intro} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="SignIn" component={SignIn} />
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import React, { ReactElement } from "react";
 import { AppBootstrap } from "@Components";
-import { Intro, Login, SignIn } from "@screens";
+import Navigator from "@config/navigator";
+// import { Intro, Login, SignIn } from "@screens";
 
 export default function App(): ReactElement {
   return (
     <AppBootstrap>
-      <Intro />
-      {/* <Login /> */}
-      {/* <SignIn /> */}
+      <Navigator />
     </AppBootstrap>
   );
 }
