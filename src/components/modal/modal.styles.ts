a8916c9bc680c28cf7236c5db101bd23dc800156
Suyeon <alveloper@gmail.com>
2021-08-18 20:33:14 +0900

    feat: create modal template

---
+++ src/components/modal/modal.styles.ts
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#ffffff",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#000",
+    borderStyle: "solid"
+  },
+  header: {},
+  text: {
+    fontSize: 20
+  },
+  body: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  footer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row"
+  }
+});
+
+export default styles;
---
+++ src/components/modal/modal.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { View } from "react-native";
+import { Text } from "@Components";
+import RNModal from "react-native-modal";
+import styles from "./modal.styles";
+
+type ModalProps = {
+  isVisible: boolean;
+  children: React.ReactNode;
+  [x: string]: any;
+};
+
+export const Modal = ({ isVisible = false, children, ...props }: ModalProps): React.ReactNode => {
+  return (
+    <RNModal
+      isVisible={isVisible}
+      animationInTiming={1000}
+      animationOutTiming={1000}
+      backdropTransitionInTiming={800}
+      backdropTransitionOutTiming={800}
+      {...props}
+    >
+      {children}
+    </RNModal>
+  );
+};
+
+const ModalContainer = ({ children }: { children: React.ReactNode }) => (
+  <View style={styles.container}>{children}</View>
+);
+
+const ModalHeader = ({ title }: { title: string }) => (
+  <View style={styles.header}>
+    <Text style={styles.text}>{title}</Text>
+  </View>
+);
+
+const ModalBody = ({ children }: { children?: React.ReactNode }) => (
+  <View style={styles.body}>{children}</View>
+);
+
+const ModalFooter = ({ children }: { children?: React.ReactNode }) => (
+  <View style={styles.footer}>{children}</View>
+);
+
+Modal.Header = ModalHeader;
+Modal.Container = ModalContainer;
+Modal.Body = ModalBody;
+Modal.Footer = ModalFooter;
