9641987d700c1bceeee39e12695515b0c8d3611a
Suyeon <alveloper@gmail.com>
2021-08-20 22:10:15 +0900

    chore: build the basic main component to make custom header

--- src/screens/main/main.tsx
+++ src/screens/main/main.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import { View, Text } from "react-native";
+import styles from "./main.style";
 
-export default function Main() {
+export default function Main(): ReactElement {
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <Text>Main page.</Text>
     </View>
   );
 }
