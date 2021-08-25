f96d742d80e7db2bea654270c29cd61aa7fce765
Suyeon <alveloper@gmail.com>
2021-08-26 00:22:37 +0900

    feat: make cups layout

--- src/screens/cups/cups.tsx
+++ src/screens/cups/cups.tsx
@@ -1,14 +1,56 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { ReactElement, useLayoutEffect } from "react";
+import { View, Image, TouchableOpacity, ScrollView } from "react-native";
+import { Text } from "@Components";
+import styles from "./cups.style";
+import { DrawerNavigationProp } from "@react-navigation/drawer";
+import { DrawerNavigationParams } from "@config/navigator";
 
-const Cups = () => {
+type NavigationProps = {
+  navigation: DrawerNavigationProp<DrawerNavigationParams, "Cups">;
+};
+
+const Cups = ({ navigation }: NavigationProps): ReactElement => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      // eslint-disable-next-line react/display-name
+      headerLeft: () => (
+        <View>
+          <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.goBack()}>
+            <Image
+              style={{ width: 11, height: 22, marginLeft: 16, marginRight: 16 }}
+              source={require("@assets/btn_back.png")}
+            />
+          </TouchableOpacity>
+        </View>
+      )
+    });
+  }, [navigation]);
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>This is Cup page.</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.myTumbler}>내 텀블러</Text>
+        <View style={styles.tumblerWrapper}>
+          <Text style={styles.tumblerFonts}>현재 텀블러</Text>
+          <Image style={styles.presentTumbler} source={require("@assets/empty-cup.png")} />
+        </View>
+        <View>
+          <Text style={styles.myTumblersFonts}>보유 텀블러</Text>
+          <View style={styles.tumblersWrapper}>
+            <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
+            <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
+            <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
+            <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
+            <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
+            <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
+            <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
+            <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
+            <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
 export default Cups;
-
-const styles = StyleSheet.create({});
