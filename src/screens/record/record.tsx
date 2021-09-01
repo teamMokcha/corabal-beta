f7968d1b24e37193651325182b798395fa0eb1ba
Suyeon <alveloper@gmail.com>
2021-09-02 03:46:20 +0900

    feat: Make record options to be manipulated by pressing each button

--- src/screens/record/record.tsx
+++ src/screens/record/record.tsx
@@ -33,6 +33,21 @@ const Record = ({ navigation }: NavigationProps): ReactElement => {
     });
   }, [navigation]);
 
+  // 화면 나갔다가 들어올 때마다 초기화
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      setOneShot(false);
+      setTwoShot(true);
+      setThreeShot(false);
+      setWater(true);
+      setMilk(false);
+      setSyrup(false);
+      setCream(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.dateFont}>2021.06.28</Text>
@@ -52,16 +67,49 @@ const Record = ({ navigation }: NavigationProps): ReactElement => {
             <Image style={styles.shotIcon} source={require("@assets/shot-of-today.png")} />
           </View>
           <View style={styles.flexDirRow}>
-            <TouchableOpacity style={styles.optionAlign}>
-              <Image style={styles.optionImg} source={require("@assets/empty-shot.png")} />
+            <TouchableOpacity
+              style={styles.optionAlign}
+              onPress={() => {
+                setOneShot(true);
+                setTwoShot(false);
+                setThreeShot(false);
+              }}
+            >
+              {oneShot ? (
+                <Image style={styles.optionImg} source={require("@assets/1shot.png")} />
+              ) : (
+                <Image style={styles.optionImg} source={require("@assets/empty-shot.png")} />
+              )}
               <Text>1샷</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.optionAlign}>
-              <Image style={styles.optionImg} source={require("@assets/empty-shot.png")} />
+            <TouchableOpacity
+              style={styles.optionAlign}
+              onPress={() => {
+                setOneShot(false);
+                setTwoShot(true);
+                setThreeShot(false);
+              }}
+            >
+              {twoShot ? (
+                <Image style={styles.optionImg} source={require("@assets/2shot.png")} />
+              ) : (
+                <Image style={styles.optionImg} source={require("@assets/empty-shot.png")} />
+              )}
               <Text>2샷</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.optionAlign}>
-              <Image style={styles.optionImg} source={require("@assets/empty-shot.png")} />
+            <TouchableOpacity
+              style={styles.optionAlign}
+              onPress={() => {
+                setOneShot(false);
+                setTwoShot(false);
+                setThreeShot(true);
+              }}
+            >
+              {threeShot ? (
+                <Image style={styles.optionImg} source={require("@assets/3shot.png")} />
+              ) : (
+                <Image style={styles.optionImg} source={require("@assets/empty-shot.png")} />
+              )}
               <Text>3샷</Text>
             </TouchableOpacity>
           </View>
@@ -70,12 +118,32 @@ const Record = ({ navigation }: NavigationProps): ReactElement => {
         <View style={styles.eachOptionContainer}>
           <Text style={styles.baseTitle}>베이스</Text>
           <View style={styles.flexDirRow}>
-            <TouchableOpacity style={styles.optionAlign}>
-              <Image style={styles.optionImg} source={require("@assets/empty-base.png")} />
+            <TouchableOpacity
+              style={styles.optionAlign}
+              onPress={() => {
+                setWater(true);
+                setMilk(false);
+              }}
+            >
+              {water && !milk ? (
+                <Image style={styles.optionImg} source={require("@assets/water-selected.png")} />
+              ) : (
+                <Image style={styles.optionImg} source={require("@assets/empty-base.png")} />
+              )}
               <Text>물</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.optionAlign}>
-              <Image style={styles.optionImg} source={require("@assets/empty-base.png")} />
+            <TouchableOpacity
+              style={styles.optionAlign}
+              onPress={() => {
+                setWater(false);
+                setMilk(true);
+              }}
+            >
+              {milk && !water ? (
+                <Image style={styles.optionImg} source={require("@assets/milk-selected.png")} />
+              ) : (
+                <Image style={styles.optionImg} source={require("@assets/empty-base.png")} />
+              )}
               <Text>우유</Text>
             </TouchableOpacity>
           </View>
@@ -84,12 +152,20 @@ const Record = ({ navigation }: NavigationProps): ReactElement => {
         <View style={styles.eachOptionContainer}>
           <Text style={styles.elseTitle}>추가</Text>
           <View style={styles.flexDirRow}>
-            <TouchableOpacity style={styles.optionAlign}>
-              <Image style={styles.optionImg} source={require("@assets/empty-base.png")} />
+            <TouchableOpacity style={styles.optionAlign} onPress={() => setSyrup(!syrup)}>
+              {syrup ? (
+                <Image style={styles.optionImg} source={require("@assets/syrup-selected.png")} />
+              ) : (
+                <Image style={styles.optionImg} source={require("@assets/empty-base.png")} />
+              )}
               <Text>시럽</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.optionAlign}>
-              <Image style={styles.optionImg} source={require("@assets/empty-cream.png")} />
+            <TouchableOpacity style={styles.optionAlign} onPress={() => setCream(!cream)}>
+              {cream ? (
+                <Image style={styles.optionImg} source={require("@assets/cream-selected.png")} />
+              ) : (
+                <Image style={styles.optionImg} source={require("@assets/empty-cream.png")} />
+              )}
               <Text>크림</Text>
             </TouchableOpacity>
           </View>
