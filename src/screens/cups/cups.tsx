918c3b8575e2691f8827388f85c773bcc378724b
Suyeon <alveloper@gmail.com>
2021-09-02 19:06:28 +0900

    refact: Change useLayoutEffect to custom header component

--- src/screens/cups/cups.tsx
+++ src/screens/cups/cups.tsx
@@ -1,55 +1,36 @@
-import React, { ReactElement, useLayoutEffect } from "react";
-import { View, Image, TouchableOpacity, ScrollView } from "react-native";
-import { Text } from "@Components";
+import React, { ReactElement } from "react";
+import { View, Image, ScrollView } from "react-native";
+import { Text, Header } from "@Components";
 import styles from "./cups.style";
-import { DrawerNavigationProp } from "@react-navigation/drawer";
-import { DrawerNavigationParams } from "@config/navigator";
 
-type NavigationProps = {
-  navigation: DrawerNavigationProp<DrawerNavigationParams, "Cups">;
-};
-
-const Cups = ({ navigation }: NavigationProps): ReactElement => {
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      // eslint-disable-next-line react/display-name
-      headerLeft: () => (
-        <View>
-          <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.goBack()}>
-            <Image
-              style={{ width: 11, height: 22, marginLeft: 16, marginRight: 16 }}
-              source={require("@assets/btn_back.png")}
-            />
-          </TouchableOpacity>
-        </View>
-      )
-    });
-  }, [navigation]);
-
+const Cups = (): ReactElement => {
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        <Text style={styles.myTumbler}>내 텀블러</Text>
-        <View style={styles.tumblerWrapper}>
-          <Text style={styles.tumblerFonts}>현재 텀블러</Text>
-          <Image style={styles.presentTumbler} source={require("@assets/empty-cup.png")} />
-        </View>
-        <View>
-          <Text style={styles.myTumblersFonts}>보유 텀블러</Text>
-          <View style={styles.tumblersWrapper}>
-            <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
-            <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
-            <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
-            <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
-            <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
-            <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
-            <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
-            <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
-            <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
+    <>
+      <Header back={true} close={false} />
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={styles.myTumbler}>내 텀블러</Text>
+          <View style={styles.tumblerWrapper}>
+            <Text style={styles.tumblerFonts}>현재 텀블러</Text>
+            <Image style={styles.presentTumbler} source={require("@assets/empty-cup.png")} />
+          </View>
+          <View>
+            <Text style={styles.myTumblersFonts}>보유 텀블러</Text>
+            <View style={styles.tumblersWrapper}>
+              <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
+              <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
+              <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
+              <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
+              <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
+              <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
+              <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
+              <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
+              <Image style={styles.tumblers} source={require("@assets/empty-cup.png")} />
+            </View>
           </View>
         </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </>
   );
 };
 
