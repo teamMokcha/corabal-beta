038a18f34ca2fe86272a61c23c002afc99749c88
Suyeon <alveloper@gmail.com>
2021-09-03 22:05:24 +0900

    feat: Create radio button layout

--- src/screens/goal/goal.tsx
+++ src/screens/goal/goal.tsx
@@ -4,8 +4,13 @@ import { Text, Modal, ButtonGradient } from "@Components";
 import { Input } from "react-native-elements";
 import styles from "./goal.style";
 
+const radioBtn = [0, 1, 2, 3, 4, 5];
+
 // props 로 넘겨주기
 const Goal = (): ReactElement => {
+  const [isShowingGoal, setIsShowingGoal] = useState(true);
+  const [selectedGoal, setSelectedGoal] = useState(0);
+
   return (
     <Modal isVisible={false}>
       <Modal.Container>
@@ -16,16 +21,32 @@ const Goal = (): ReactElement => {
           </TouchableOpacity>
         </View>
         <Modal.Body>
-          <Text style={styles.bodyFont}>구체적으로 목표를 설정을 해보세요.</Text>
-          <View style={styles.aimWrapper}>
-            <Text style={styles.aim}>1일</Text>
-            <TextInput style={styles.aimInput} placeholder="00" keyboardType="number-pad" />
-            <Text style={styles.aim}>잔</Text>
+          <Text style={styles.bodyFont}>구체적으로 목표를 설정해보세요.</Text>
+          <View style={{ flexDirection: "row" }}>
+            {radioBtn.map(btn => {
+              return (
+                // eslint-disable-next-line react/jsx-key
+                <TouchableOpacity
+                  style={{ alignItems: "center", margin: 5 }}
+                  onPress={() => setSelectedGoal(btn)}
+                >
+                  <Image source={require("@assets/header-profile.png")} />
+                  <Text>{btn}잔</Text>
+                </TouchableOpacity>
+              );
+            })}
           </View>
-          <Text style={styles.caution}>* 하루 권장량 1일 1잔</Text>
+          <Text style={styles.caution}>하루 권장량 1일 1잔</Text>
         </Modal.Body>
         <Modal.Footer>
-          <ButtonGradient style={styles.complete} title="완료" />
+          <ButtonGradient
+            onPress={() => {
+              setIsShowingGoal(false);
+              console.log(selectedGoal);
+            }}
+            style={styles.complete}
+            title="완료"
+          />
         </Modal.Footer>
       </Modal.Container>
     </Modal>
